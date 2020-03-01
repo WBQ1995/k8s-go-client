cfefcfbe4153ade523ca7b826ade3d62ab8d41fb
boqian <boqianwang1995@gmail.com>
2020-03-01 18:56:12 -0500

    Fix spelling problem.

--- common/errors/errorMessage.go
+++ common/errors/errorMessage.go
@@ -7,10 +7,10 @@ const (
 	ErrorMsgHttpCode500InternalServerError ErrorMessage = "Internal error, please try again."
 
 	//k8s error
-	ErrorMsgK8sListNodes 		 = "An error occurs wile listing k8s nodes."
-	ErrorMsgK8sListPods  		 = "An error occurs wile listing k8s pods."
-	ErrorMsgK8sCreateDeployment  = "An error occurs wile creating a k8s deployment."
-	ErrorMsgK8sDeleteDeployment  = "An error occurs wile deleting a k8s deployment."
-	ErrorMsgK8sCreateService	 = "An error occurs wile creating a k8s service."
-	ErrorMsgK8sDeleteService	 = "An error occurs wile deleting a k8s service."
+	ErrorMsgK8sListNodes        = "An error occurs while listing k8s nodes."
+	ErrorMsgK8sListPods         = "An error occurs while listing k8s pods."
+	ErrorMsgK8sCreateDeployment = "An error occurs while creating a k8s deployment."
+	ErrorMsgK8sDeleteDeployment = "An error occurs while deleting a k8s deployment."
+	ErrorMsgK8sCreateService    = "An error occurs while creating a k8s service."
+	ErrorMsgK8sDeleteService    = "An error occurs while deleting a k8s service."
 )
